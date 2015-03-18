f3466b1733b23e060042a7b1625ea332f9053796
Julian <julian.alonso.dev@gmail.com>
2015-03-18 19:50:39 +0100

    Added ListCollectionViewController

--- KinoApp/ViewControllers/ListsCollectionViewController.h
+++ KinoApp/ViewControllers/ListsCollectionViewController.h
@@ -1,15 +1,13 @@
 //
-//  FetchAllFilmsInteractor.h
+//  ListsCollectionViewController.h
 //  KinoApp
 //
 //  Created by Julian Alonso on 18/3/15.
 //  Copyright (c) 2015 Julian. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface FetchAllFilmsInteractor : NSObject
-
-
+@interface ListsCollectionViewController : UIViewController
 
 @end
