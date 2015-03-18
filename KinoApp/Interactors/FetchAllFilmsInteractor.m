56103c68f750ac2df3da3cd904d6a32772de6925
Julian <julian.alonso.dev@gmail.com>
2015-03-18 19:49:16 +0100

    Added fetchAllFilmsInteractor

---
+++ KinoApp/Interactors/FetchAllFilmsInteractor.m
@@ -0,0 +1,13 @@
+//
+//  FetchAllFilmsInteractor.m
+//  KinoApp
+//
+//  Created by Julian Alonso on 18/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import "FetchAllFilmsInteractor.h"
+
+@implementation FetchAllFilmsInteractor
+
+@end
