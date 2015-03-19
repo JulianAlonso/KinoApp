4c29208c20e34b2906dde89e77cee134ca772bdd
Julian <julian.alonso.dev@gmail.com>
2015-03-19 18:38:28 +0100

    Added SelectListRouter

---
+++ KinoApp/Routers/SelectListRouter.h
@@ -0,0 +1,13 @@
+//
+//  SelectListRouter.h
+//  KinoApp
+//
+//  Created by Julian Alonso on 19/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SelectListRouter : NSObject
+
+@end
---
+++ KinoApp/Routers/SelectListRouter.m
@@ -0,0 +1,13 @@
+//
+//  SelectListRouter.m
+//  KinoApp
+//
+//  Created by Julian Alonso on 19/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import "SelectListRouter.h"
+
+@implementation SelectListRouter
+
+@end
