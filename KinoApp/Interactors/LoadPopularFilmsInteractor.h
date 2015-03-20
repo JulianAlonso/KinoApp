bf708ca3c1f66b72ef0b7c3f624243beb9a2461e
Julian <julian.alonso.dev@gmail.com>
2015-03-20 15:55:07 +0100

    Added loadPopularFilmsInteractor

---
+++ KinoApp/Interactors/LoadPopularFilmsInteractor.h
@@ -0,0 +1,13 @@
+//
+//  LoadPopularFilmsInteractor.h
+//  KinoApp
+//
+//  Created by Julian Alonso on 20/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoadPopularFilmsInteractor : NSObject
+
+@end
---
+++ KinoApp/Interactors/LoadPopularFilmsInteractor.m
@@ -0,0 +1,13 @@
+//
+//  LoadPopularFilmsInteractor.m
+//  KinoApp
+//
+//  Created by Julian Alonso on 20/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import "LoadPopularFilmsInteractor.h"
+
+@implementation LoadPopularFilmsInteractor
+
+@end
