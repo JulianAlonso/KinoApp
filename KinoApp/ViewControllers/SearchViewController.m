db58f0ae95ac19efa8748f17198a27affd28a60f
Julian <julian.alonso.dev@gmail.com>
2015-03-20 15:53:16 +0100

    Added searchViewController

---
+++ KinoApp/ViewControllers/SearchViewController.m
@@ -0,0 +1,42 @@
+//
+//  SearchViewController.m
+//  KinoApp
+//
+//  Created by Julian Alonso on 20/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController ()
+
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UITableView *searchTableView;
+
+@end
+
+@implementation SearchViewController
+
+#pragma mark - Lifecycle methods.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Config methods.
+- (void)configSearchBar
+{
+    
+}
+
+- (void)configSearchTableView
+{
+    
+}
+
+@end
