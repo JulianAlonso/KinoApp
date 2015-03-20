f1987ff95a626bbe2e86af2fe055242f7f37e837
Julian <julian.alonso.dev@gmail.com>
2015-03-20 16:05:14 +0100

    Added searchFilmTableViewCellController

---
+++ KinoApp/Controllers/SearchFilmTableViewCellController.h
@@ -0,0 +1,13 @@
+//
+//  SearchFilmTableViewCellController.h
+//  KinoApp
+//
+//  Created by Julian Alonso on 20/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SearchFilmTableViewCellController : NSObject
+
+@end
---
+++ KinoApp/Controllers/SearchFilmTableViewCellController.m
@@ -0,0 +1,23 @@
+//
+//  SearchFilmTableViewCellController.m
+//  KinoApp
+//
+//  Created by Julian Alonso on 20/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import "SearchFilmTableViewCellController.h"
+#import "FilmTableViewCell.h"
+
+@interface SearchFilmTableViewCellController () <FilmTableViewCellDelegate>
+
+@end
+
+@implementation SearchFilmTableViewCellController
+
+- (void)filmTableViewCell:(FilmTableViewCell *)filmTableViewCell didTapWithSender:(UITapGestureRecognizer *)sender
+{
+    
+}
+
+@end
