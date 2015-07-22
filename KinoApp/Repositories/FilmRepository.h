54d3e759af6008bf77bd0dca92afd13e108b9da6
Julian <julian.alonso.dev@gmail.com>
2015-07-22 13:37:21 +0200

    Created films and lists repositories

---
+++ KinoApp/Repositories/FilmRepository.h
@@ -0,0 +1,13 @@
+//
+//  FilmRepository.h
+//  KinoApp
+//
+//  Created by Julian Alonso on 22/7/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FilmRepository <NSObject>
+
+@end
---
+++ KinoApp/Repositories/ListRepository.h
@@ -0,0 +1,13 @@
+//
+//  ListRepository.h
+//  KinoApp
+//
+//  Created by Julian Alonso on 22/7/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ListRepository <NSObject>
+
+@end
