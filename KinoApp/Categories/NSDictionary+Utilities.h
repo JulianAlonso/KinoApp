3dce7a440130738e25529bf25ee4e2cc672b3bcd
Julian <julian.alonso.dev@gmail.com>
2015-11-02 22:38:06 +0100

    Added a dictionary utils to get string null or filled

---
+++ KinoApp/Categories/NSDictionary+Utilities.h
@@ -0,0 +1,15 @@
+//
+//  NSDictionary+Utilities.h
+//  KinoApp
+//
+//  Created by Julian Alonso on 2/11/15.
+//  Copyright © 2015 Julian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Utilities)
+
+- (NSString *)stringWithKey:(NSString *)key;
+
+@end
---
+++ KinoApp/Categories/NSDictionary+Utilities.m
@@ -0,0 +1,23 @@
+//
+//  NSDictionary+Utilities.m
+//  KinoApp
+//
+//  Created by Julian Alonso on 2/11/15.
+//  Copyright © 2015 Julian. All rights reserved.
+//
+
+#import "NSDictionary+Utilities.h"
+
+@implementation NSDictionary (Utilities)
+
+- (NSString *)stringWithKey:(NSString *)key
+{
+    NSString *value;
+    if (self[key])
+    {
+        value = [NSString stringWithFormat:@"%@", self[key]];
+    }
+    return value;
+}
+
+@end
